95cf0991fc91d7dfcd3502de98a2434568f76aea
김영우 <kyw05171@gmail.com>
2021-09-12 15:53:44 +0900

    01-19 오류 페이지와 홈페이지 구현 #6

--- main.js
+++ main.js
@@ -1,52 +1,77 @@
 const http = require("http");
-const fs = require("fs");
 const url = require("url");
+const fs = require("fs");
 
 const app = http.createServer((request, response) => {
-  let _url = request.url;
-  let { id: title } = url.parse(_url, true).query;
+  const _url = request.url;
+  const queryData = url.parse(_url, true).query;
+  const pathname = url.parse(_url, true).pathname;
 
-  if(_url === "/") {
-    title = "Welcome";
-  }
+  if(pathname === "/") {
+    if(queryData.id === undefined) {
+      const title = "Welcome";
+      const description = "Hello, NodeJS";
 
-  if(_url === "/favicon.ico") {
-    return response.writeHead(404);
-  }
-
-  console.log(`_url: ${_url}`);
-
-  response.writeHead(200);
-
-  fs.readFile(
-    `./data/${title}`,
-    { encoding: "utf-8" },
-    (err, data) => {
       const template = `
-        <!doctype html>
-          <html>
+        <!DOCTYPE html>
           <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
+            <meta charset="UTF-8">
+            <title>${title}</title>
           </head>
 
           <body>
             <h1><a href="/">WEB</a></h1>
-            
             <ul>
               <li><a href="/?id=HTML">HTML</a></li>
               <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=Javascript">JavaScript</a></li>
+              <li><a href="/?id=Javascript">Javascript</a></li>
             </ul>
+
             <h2>${title}</h2>
-            <p>${data}</p>
+            <p>${description}</p>
           </body>
         </html>
       `;
 
+      response.writeHead(200);
       response.end(template);
-    },
-  );
+    } else {
+      fs.readFile(
+        `./data/${queryData.id}`,
+        { encoding: "utf-8" },
+        (error, description) => {
+          const title = queryData.id;
+
+          const template = `
+            <!DOCTYPE html>
+              <head>
+                <meta charset="UTF-8">
+                <title>WEB - ${title}</title>
+              </head>
+
+              <body>
+                <h1><a href="/">WEB</a></h1>
+                <ul>
+                  <li><a href="/?id=HTML">HTML</a></li>
+                  <li><a href="/?id=CSS">CSS</a></li>
+                  <li><a href="/?id=Javascript">Javascript</a></li>
+                </ul>
+
+                <h2>${title}</h2>
+                <p>${description}</p>
+              </body>
+            </html>
+          `;
+
+          response.writeHead(200);
+          response.end(template);
+        },
+      );
+    }
+  } else {
+    response.writeHead(404);
+    response.end("Not Found");
+  }
 });
 
 app.listen(80);
