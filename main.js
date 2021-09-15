2de93cdd3990192ef0eec39d1b44aa74ab807f7a
김영우 <kyw05171@gmail.com>
2021-09-15 23:44:37 +0900

    01-26 App - 함수를 이용해 코드 정리하기 #9

--- main.js
+++ main.js
@@ -2,6 +2,36 @@ const http = require("http");
 const url = require("url");
 const fs = require("fs");
 
+function templateHTML(title, list, body) {
+  return `
+    <!DOCTYPE html>
+      <head>
+        <meta charset="UTF-8">
+        <title>WEB1 - ${title}</title>
+      </head>
+
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        ${list}
+        ${body}
+      </body>
+    </html>
+  `;
+}
+
+function templateList(fileList) {
+  let list = "<ul>";
+
+  fileList.forEach(fileName => {
+    list = list.concat(`
+      <li><a href="/?id=${fileName}">${fileName}</a></li>
+    `);
+  });
+
+  list = list.concat("</ul>");
+  return list;
+}
+
 const app = http.createServer((request, response) => {
   const _url = request.url;
   const queryData = url.parse(_url, true).query;
@@ -13,66 +43,22 @@ const app = http.createServer((request, response) => {
         const title = "Welcome";
         const description = "Hello, NodeJS";
 
-        let list = "<ul>";
-        fileList.forEach(fileName => {
-          list += `<li>
-            <a href="/?id=${fileName}">${fileName}</a>
-          </li>`
-        });        
-        list += "</ul>";
-
-        const template = `
-          <!DOCTYPE html>
-            <head>
-              <meta charset="UTF-8">
-              <title>${title}</title>
-            </head>
-
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              ${list}
-
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-          </html>
-        `;
-
+        const list = templateList(fileList);
+        const template = templateHTML(title, list, description);
+        
         response.writeHead(200);
         response.end(template);
       });
     } else {
       fs.readdir("./data", (error, fileList) => {
-        let list = "<ul>";
-        fileList.forEach(fileName => {
-          list += `<li>
-            <a href="/?id=${fileName}">${fileName}</a>
-          </li>`;
-        });
-        list += "</ul>";
+        const list = templateList(fileList);
 
         fs.readFile(
           `./data/${queryData.id}`,
           { encoding: "UTF-8" },
           (error, description) => {
-            const title = queryData.id;
-            
-            const template = `
-              <!DOCTYPE html>
-                <head>
-                  <meta charset="UTF-8">
-                  <title>WEB</title>
-                </head>
-
-                <body>
-                  <h1><a href="/">WEB</a></h1>
-                  ${list}
-
-                  <h2>${title}</h2>
-                  <p>${description}</p>
-                </body>
-              </html>
-            `;
+            const title = queryData.id;            
+            const template = templateHTML(title, list, description);
 
             response.writeHead(200);
             response.end(template);
