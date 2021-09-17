d2a007621b38014fbcf69df3cd346c1b50e8e783
김영우 <kyw05171@gmail.com>
2021-09-17 21:40:38 +0900

    01-34 글 수정 (수정 링크 생성) #14

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const url = require("url");
 const qs = require("querystring");
 const fs = require("fs");
 
-function templateHTML(title, list, body) {
+function templateHTML(title, list, body, controll) {
   return `
     <!DOCTYPE html>
       <head>
@@ -14,7 +14,7 @@ function templateHTML(title, list, body) {
       <body>
         <h1><a href="/">WEB</a></h1>
         ${list}
-        <br/><a href="/create">create</a><br/>
+        ${controll}
         ${body}
       </body>
     </html>
@@ -46,7 +46,12 @@ const app = http.createServer((request, response) => {
         const description = "Hello, NodeJS";
 
         const list = templateList(fileList);
-        const template = templateHTML(title, list, description);
+        const template = templateHTML(
+          title, 
+          list, 
+          `<h2>${title}</h2><p>${description}</p>`,
+          `<a href="/create">create</a>`,
+        );
         
         response.writeHead(200);
         response.end(template);
@@ -59,8 +64,13 @@ const app = http.createServer((request, response) => {
           `./data/${queryData.id}`,
           { encoding: "UTF-8" },
           (error, description) => {
-            const title = queryData.id;            
-            const template = templateHTML(title, list, description);
+            const title = queryData.id;
+            const template = templateHTML(
+              title, 
+              list,
+              `<h2>${title}</h2><p>${description}</p>`,
+              `<a href="/create">create</a> | <a href="/update?id=${title}">update</a>`,
+            );
 
             response.writeHead(200);
             response.end(template);
@@ -72,13 +82,18 @@ const app = http.createServer((request, response) => {
     fs.readdir("./data", (error, fileList) => {
       const title = "WEB - create";
       const list = templateList(fileList);
-      const template = templateHTML(title, list, `
-        <form action="http://localhost/create_process" method="post">
-          <p><input type="text" name="title" placeholder="title"></p>
-          <p><textarea name="description" placeholder="description"></textarea></p>
-          <p><input type="submit"></p>
-        </form>
-      `);
+      const template = templateHTML(
+        title, 
+        list, 
+        `
+          <form action="http://localhost/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p><textarea name="description" placeholder="description"></textarea></p>
+            <p><input type="submit"></p>
+          </form>
+        `,
+        "",
+      );
 
       response.writeHead(200);
       response.end(template);
