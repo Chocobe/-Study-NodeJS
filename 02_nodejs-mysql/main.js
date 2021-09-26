0aa8cd6d93535499effabf0b143858e996ab6007
김영우 <kyw05171@gmail.com>
2021-09-27 01:15:59 +0900

    02-07 MySQL 로 글 수정 기능 구현

--- 02_nodejs-mysql/main.js
+++ 02_nodejs-mysql/main.js
@@ -204,47 +204,114 @@ var app = http.createServer(function(request, response) {
         //     });
         // });
     } else if(pathname === '/update') {
-        fs.readdir('./data', function(error, filelist) {
-            var filteredId = path.parse(queryData.id).base;
-            fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
-                var title = queryData.id;
-                var list = template.list(filelist);
-                var html = template.HTML(title, list,
-                    `
+        db.query(
+          `SELECT * FROM topic`,
+          (error, topics) => {
+            if(error) {
+              throw error;
+            }
+
+            db.query(
+              `
+                SELECT * FROM topic WHERE id=?
+              `,
+              [queryData.id],
+              (error2, topic) => {
+                const list = template.list(topics);
+                const html = template.HTML(
+                  topic[0].title,
+                  list,
+                  `
                     <form action="/update_process" method="post">
-                        <input type="hidden" name="id" value="${title}">
-                        <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-                        <p>
-                            <textarea name="description" placeholder="description">${description}</textarea>
-                        </p>
-                        <p>
-                            <input type="submit">
-                        </p>
+                      <input type="hidden" name="id" value="${topic[0].id}" />
+                      <p>
+                        <input type="text" name="title" placeholder="title" value="${topic[0].title}" />
+                      </p>
+                      <p>
+                        <textarea name="description" placeholder="description">${topic[0].description}</textarea>
+                      </p>
+                      <p>
+                        <input type="submit" value="Submit" />
+                      </p>
                     </form>
-                    `,
-                    `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+                  `,
+                  `
+                    <a href="/create">create</a>
+                    <a href="/update?id=${topic[0].id}">update</a>
+                  `,
                 );
+
                 response.writeHead(200);
                 response.end(html);
-            });
-        });
+              },
+            );
+          },
+        );
+        
+        // fs.readdir('./data', function(error, filelist) {
+        //     var filteredId = path.parse(queryData.id).base;
+        //     fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
+        //         var title = queryData.id;
+        //         var list = template.list(filelist);
+        //         var html = template.HTML(title, list,
+        //             `
+        //             <form action="/update_process" method="post">
+        //                 <input type="hidden" name="id" value="${title}">
+        //                 <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+        //                 <p>
+        //                     <textarea name="description" placeholder="description">${description}</textarea>
+        //                 </p>
+        //                 <p>
+        //                     <input type="submit">
+        //                 </p>
+        //             </form>
+        //             `,
+        //             `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+        //         );
+        //         response.writeHead(200);
+        //         response.end(html);
+        //     });
+        // });
     } else if(pathname === '/update_process') {
-        var body = '';
-        request.on('data', function(data) {
-            body = body + data;
+        let body = "";
+
+        request.on("data", data => {
+          body = body.concat(data);
         });
-        request.on('end', function() {
-            var post = qs.parse(body);
-            var id = post.id;
-            var title = post.title;
-            var description = post.description;
-            fs.rename(`data/${id}`, `data/${title}`, function(error) {
-                fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
-                    response.writeHead(302, {Location: `/?id=${title}`});
-                    response.end();
-                });
-            });
+
+        request.on("end", () => {
+          const { title, description, id } = qs.parse(body);
+
+          db.query(
+            `
+              UPDATE topic
+              SET title=?, description=?, author_id=1
+              WHERE id=?
+            `,
+            [title, description, id],
+            (error, result) => {
+              response.writeHead(302, { Location: `/?id=${id}`});
+              response.end();
+            },
+          );
         });
+      
+        // var body = '';
+        // request.on('data', function(data) {
+        //     body = body + data;
+        // });
+        // request.on('end', function() {
+        //     var post = qs.parse(body);
+        //     var id = post.id;
+        //     var title = post.title;
+        //     var description = post.description;
+        //     fs.rename(`data/${id}`, `data/${title}`, function(error) {
+        //         fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
+        //             response.writeHead(302, {Location: `/?id=${title}`});
+        //             response.end();
+        //         });
+        //     });
+        // });
     } else if(pathname === '/delete_process') {
         var body = '';
         request.on('data', function(data) {
